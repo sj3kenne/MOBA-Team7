67cef5aee3d74ceb4a6ea9de29af6295bb951927
sheiladumlao <smdumlao@uwaterloo.ca>
2017-10-15 14:03:09 -0400

    changed name of table files, all point to my_connect.php
    File names of php tables are changed and only need to change your db name on the my_connect.php file instead of every file

--- table-indicators.php
+++ table-indicators.php
@@ -1,35 +1,12 @@
 <?php
 
-$local_host = "localhost";
-$local_username = "root";
-$local_password = "root";
-$local_databaseName = "MOAB_garvita";
+// Enable error logging: 
+error_reporting(E_ALL ^ E_NOTICE);
 
-
-// Production Database Connection Details:
-//$databaseConnectURL = "mysql://bec9224a3c2850:feb6f6a3@us-cdbr-iron-east-04.cleardb.net/heroku_81f997698bd0911?reconnect=true";
-// CLEARDB_DATABASE_URL needs to be set in Heroku.  
-$databaseConnectURL = getenv("CLEARDB_DATABASE_URL");
-
-$possibleLocalhosts = array('127.0.0.1', "::1");
-if(in_array($_SERVER['REMOTE_ADDR'], $possibleLocalhosts)) // If our REMOTE_ADDR is a localhost, do this:
-{
-	// Open a connection with our local database
-	$todoAppMySQLConnection = mysqli_connect($local_host, $local_username, $local_password, $local_databaseName);
-} 
-
-else // If our REMOTE_ADDR wasn't a localhost, we must be working remotely.
-{ 
-	// Parse our $databaseConnectURL so that we can pull out the key's we neeed
-	$parsedDatabaseConnectUrl = parse_url($databaseConnectURL);
-	$remote_host = $parsedDatabaseConnectUrl["host"];
-	$remote_username = $parsedDatabaseConnectUrl["user"];
-	$remote_password = $parsedDatabaseConnectUrl["pass"];
-	$remote_databaseName = substr($parsedDatabaseConnectUrl["path"], 1);
-
-	// Open a connection with our remote database
-	$todoAppMySQLConnection = mysqli_connect($remote_host, $remote_username, $remote_password, $remote_databaseName);
-}
+// mysqli connection via user-defined function
+include('./my_connect.php');
+$mysqli = get_mysqli_conn();
+$ID = $_GET['ID'];
 
 //sql coe (Done by Sam)
 
@@ -56,9 +33,9 @@ VALUES (1, 'Recognize the key elements of object-oriented programming using Java
 (16,'Generate an organized and professional report with appropriate page length, section headings, and/or correct spelling and grammar'),
 (17,'Prepare and present a solution to a real-life problem')";
     
-$stmt1= $todoAppMySQLConnection-> prepare ($sql_CreateIndicators);
+$stmt1= $mysqli-> prepare ($sql_CreateIndicators);
 $stmt1->execute (); 
-$stmt2= $todoAppMySQLConnection-> prepare ($sql_InsertIndicators);
+$stmt2= $mysqli-> prepare ($sql_InsertIndicators);
 $stmt2->execute (); 
 
 ?>
