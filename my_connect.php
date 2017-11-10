457f35eb1eb6a36667a8a202901948222e4eca5c
Gordon <g4lau@edu.uwaterloo.ca>
2017-11-10 16:08:26 -0500

    changing-db

--- my_connect.php
+++ my_connect.php
@@ -5,8 +5,7 @@ function get_mysqli_conn()
     $local_host = "localhost";
 $local_username = "root";
 $local_password = "root";
-
-$local_databaseName = "chris";
+$local_databaseName = "g4lau-msci342-local-db";
 
 
 // Production Database Connection Details:
