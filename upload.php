59218e70db943b1d1479a967f3c72ad7db5d9ff9
JuanaAtt <j2attieh@edu.uwaterloo.ca>
2017-11-14 00:06:31 -0500

    Upload changes

--- upload.php
+++ upload.php
@@ -1,3 +1,23 @@
+<html>
+    <head>
+       <title>Graph Filters</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+<header>
+	M-OBA
+	<img src="waterlooLogo.png" style="height:100%;float:right;"/>
+</header>
+<body>
+    <div id="sidebar">
+            <ul>
+                <li><a href="sidebar.html" class="fa fa-home" style="font-size:30px;color:#ccc;"></a></li>
+                <li><a href="filter-for-graphs.php" class="active">Analytics</a></li>
+                <li><a href="filter-for-tables.php">Raw Data</a></li>
+                <li><a href="uploading.html">Import</a></li>
+            </ul>
+        </div>
+    <h2> Import Status </h2>
 <?php
 // Enable error logging: 
 error_reporting(E_ALL ^ E_NOTICE);
@@ -18,22 +38,22 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 }
 // Allow certain file formats
 if($csvFileType != "csv") {
-    echo "<br> Please upload CSV files. Other file types are not allowed. </br>";
+    echo "<h3> Please upload CSV files. Other file types are not allowed. </h3>";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "<br> Upload Incomplete </br>";
+    echo "<h3> Upload Incomplete </h3>";
     
 // if everything is ok, update db
 } else {
     $contents = file_get_contents($_FILES["fileToUpload"]["tmp_name"]);
-    echo($contents);
+    //echo($contents);
     $grad = preg_split('/\s+/', $contents);
-    print_r($grad);
-    echo(count($grad));
+    //print_r($grad);
+    //echo(count($grad));
     if(count($grad) == 0){
-        echo('No students in file');
+        echo('<h3> No students in file </h3>');
     }
     elseif(count($grad) > 0){
         
@@ -54,17 +74,22 @@ if ($uploadOk == 0) {
     {
       $studentlist = $studentlist . "," . $Students_StudentID;
 }
-     $studentlist = $studentlist . "";   
+     $studentlist = $studentlist . ",";   
   for ($i = 0; $i < count($grad); ++$i) { 
-    if (strpos($studentlist,'"' . $grad[$i] . '"') !== false) {
+      //echo($studentlist);
+    if (strpos($studentlist,',' . $grad[$i] . ',') !== false) {
     $sql1 = "UPDATE Students SET GradYear = (SELECT EXTRACT(YEAR FROM CURRENT_DATE)) WHERE StudentID = " . $grad[$i];
     $stmt1= $mysqli-> prepare ($sql1);
     $stmt1->execute (); 
    } else {
-            echo('</br> Student ID ' . $grad[$i] . ' is not in current list of students.</br>');
+            echo('<h3> Student ID ' . $grad[$i] . ' is not in current list of students.</h3>');
     } 
     }
-        echo(' </br> Graduation year for other students in list updated successfully to this year </br>' );
+        echo(' <h3> Graduation year for other students in list updated successfully to this year </h3>' );
 }
 }
 ?>
+        <div class="tron">
+      </div>
+</body>
+</html>
