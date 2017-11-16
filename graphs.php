17078a45bf5045815b9464490aabcbd0c67b4a76
sj3kenne <32079919+sj3kenne@users.noreply.github.com>
2017-11-16 16:42:03 -0500

    added multi-coloured bars
    just cause

--- graphs.php
+++ graphs.php
@@ -333,10 +333,51 @@ $mysqli->close();
     
             <svg class="chart"></svg>
             <script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script>
+    
             <script>
-            
-                
-                
+            // Adapted from http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+
+            var randomColour = (function(){
+              var golden_ratio_conjugate = 0.618033988749895;
+              var h = Math.random();
+
+              var hslToRgb = function (h, s, l){
+                  var r, g, b;
+
+                  if(s == 0){
+                      r = g = b = l; // achromatic
+                  }else{
+                      function hue2rgb(p, q, t){
+                          if(t < 0) t += 1;
+                          if(t > 1) t -= 1;
+                          if(t < 1/6) return p + (q - p) * 6 * t;
+                          if(t < 1/2) return q;
+                          if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                          return p;
+                      }
+
+                      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+                      var p = 2 * l - q;
+                      r = hue2rgb(p, q, h + 1/3);
+                      g = hue2rgb(p, q, h);
+                      b = hue2rgb(p, q, h - 1/3);
+                  }
+
+                  return '#'+Math.round(r * 255).toString(16)+Math.round(g * 255).toString(16)+Math.round(b * 255).toString(16);
+              };
+
+              return function(){
+                h += golden_ratio_conjugate;
+                h %= 1;
+                return hslToRgb(h, 0.5, 0.60);
+              };
+            })();
+            </script>
+    
+    
+    
+    
+            <script>    
             function graphs(stringtosplitA, stringtosplitB, histotitle, progtitle){
                 //alert("function begins");  
 
@@ -399,6 +440,7 @@ $mysqli->close();
                       .attr("y", function(d) { return y(d); })
                       .attr("height", function(d) { return height - y(d); })
                       .attr("width", x.rangeBand() - barPadding)
+                      .style({fill: randomColour});
                 //Bar Text
                 chart.selectAll("g")
                     .data(histogramarray)
@@ -485,7 +527,8 @@ $mysqli->close();
                       .attr("y", function(d) { return y2(d); })
                       .attr("height", function(d) { return height2 - y2(d); })
                       .attr("width", x2.rangeBand() - barPadding)
-                //bartext
+                    .style({fill: randomColour});
+                //Bar text
                 chart2.selectAll("g")
                     .data(progressionarray)
                     //.enter()
