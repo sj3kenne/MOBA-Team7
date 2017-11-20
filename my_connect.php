898128cd32534cab2fc8e5daccdaeb43464e406c
GarvitaR <32111050+GarvitaR@users.noreply.github.com>
2017-11-19 21:15:14 -0500

    update student upload files, code commented

--- my_connect.php
+++ my_connect.php
@@ -6,7 +6,7 @@ function get_mysqli_conn()
 $local_username = "root";
 $local_password = "root";
 
-$local_databaseName = "samk";
+$local_databaseName = "upload";
 
 
 // Production Database Connection Details:
