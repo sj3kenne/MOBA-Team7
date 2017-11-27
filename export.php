66a7a8b1d136439ef2825887b811162a87992dae
GarvitaR <32111050+GarvitaR@users.noreply.github.com>
2017-11-27 11:32:12 -0500

    export- added student column

--- export.php
+++ export.php
@@ -15,7 +15,7 @@ $mysqli = get_mysqli_conn();
 $stmt = $mysqli->prepare($sql);
 // Prepared statement, stage 2: execute
 $stmt->execute();
-$stmt->bind_result($ScoreUsedFor_courseName,$ScoreUsedFor_AttributeName, $ScoreUsedFor_IndicatorName, $ScoreUsedFor_score);
+$stmt->bind_result($ScoreUsedFor_courseName, $ScoreUsedFor_StudentID,$ScoreUsedFor_AttributeName, $ScoreUsedFor_IndicatorName, $ScoreUsedFor_score);
 
 //initialize excel file
 $file_ending = "xls";
@@ -30,6 +30,7 @@ $sep = "\t"; //tabbed character
 //start of printing column names as names of MySQL fields
    
 echo "Course \t";
+echo "Student ID \t";
 echo "Attribute \t";
 echo "Indicator \t";
 echo  "score \t";
@@ -40,8 +41,7 @@ print("\n");
     while($stmt->fetch())
     {
         //print each line of the code and then go to the next line
-        echo  $ScoreUsedFor_courseName;
-        $schema_insert = $ScoreUsedFor_courseName . "\t" . $ScoreUsedFor_AttributeName. "\t" . $ScoreUsedFor_IndicatorName. "\t" . $ScoreUsedFor_score;
+        $schema_insert = $ScoreUsedFor_courseName . "\t" . $ScoreUsedFor_StudentID  . "\t" . $ScoreUsedFor_AttributeName. "\t" . $ScoreUsedFor_IndicatorName. "\t" . $ScoreUsedFor_score;
         
         $schema_insert = str_replace($sep."$", "", $schema_insert);
         $schema_insert = preg_replace("/\r\n|\n\r|\n|\r/", " ", $schema_insert);
